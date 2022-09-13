14aa337135dabbf2e37bfdf8ea34667273c6f420
cr <clint.rogers@oit.edu>
2022-09-13 13:38:59 -0700

    Fixeed 'fullscree'

--- godot/timestamp.gd
+++ godot/timestamp.gd
@@ -1,3 +1,3 @@
 extends Node2D;
-const BUILD_STAMP=" git: 59401447382d "
-const BUILD_DATE="date: 2022-09-13T01:02:56-0700 "
+const BUILD_STAMP=" git: 4dec3814c1d0 "
+const BUILD_DATE="date: 2022-09-13T13:36:01-0700 "
