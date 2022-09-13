a22375050c16e2d208ba28e8c29c692574aa3718
cr <clint.rogers@oit.edu>
2022-09-13 13:36:01 -0700

    - Sub navigation works again. - added some tuneable vars to control acceleration, animation, etc. - verified that the jelly works in game (but not the discovery mechanism yet, there's no code for that yet)
    NOTE: the ocean shader's still broken

--- godot/timestamp.gd
+++ godot/timestamp.gd
@@ -1,3 +1,3 @@
 extends Node2D;
-const BUILD_STAMP=" git: 5be80e6e7b3c "
-const BUILD_DATE="date: 2022-09-12T10:20:09-0700 "
+const BUILD_STAMP=" git: 59401447382d "
+const BUILD_DATE="date: 2022-09-13T01:02:56-0700 "
