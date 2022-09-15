1cf3c3a16d0d8a4b62dc43bd4072de7e0992cc49
cr <clint.rogers@oit.edu>
2022-09-14 19:12:26 -0700

    - improved water, added forward-firing lamps to submarine - possible performance regression, more research is needed to find out why

--- godot/timestamp.gd
+++ godot/timestamp.gd
@@ -1,3 +1,3 @@
 extends Node2D;
-const BUILD_STAMP=" git: a22375050c16 "
-const BUILD_DATE="date: 2022-09-13T13:38:59-0700 "
+const BUILD_STAMP=" git: 43c8017a4c7d "
+const BUILD_DATE="date: 2022-09-14T17:44:59-0700 "
