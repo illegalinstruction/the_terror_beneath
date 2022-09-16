58a39273c81d2bfd30174caaf0138cebd8f5407f
cr <clint.rogers@oit.edu>
2022-09-15 23:44:05 -0700

    reimport the subsurface gradient images without s3tc compression (reduces banding, but doesn't cost much in vram, since they're both 4x256)

--- godot/timestamp.gd
+++ godot/timestamp.gd
@@ -1,3 +1,3 @@
 extends Node2D;
-const BUILD_STAMP=" git: 1403b581175c "
-const BUILD_DATE="date: 2022-09-15T17:49:09-0700 "
+const BUILD_STAMP=" git: 5aa946cd5f55 "
+const BUILD_DATE="date: 2022-09-15T23:28:44-0700 "
