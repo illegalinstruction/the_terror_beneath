7c619c7006fde8deab2d638dce62d580e8dcbf6b
cr <clint.rogers@oit.edu>
2022-09-19 20:48:32 -0700

    Button callouts for the in-game HUD

--- godot/timestamp.gd
+++ godot/timestamp.gd
@@ -1,3 +1,3 @@
 extends Node2D;
-const BUILD_STAMP=" git: 23b1f2a17cd5 "
-const BUILD_DATE="date: 2022-09-19T18:07:34-0700 "
+const BUILD_STAMP=" git: 7f4888163e95 "
+const BUILD_DATE="date: 2022-09-19T18:11:00-0700 "
